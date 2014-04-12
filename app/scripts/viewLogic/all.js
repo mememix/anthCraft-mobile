cae82adbc2d05bc5b6c2ccfd2c372c74a680fbc0
zhang-ning <hunter.dding@gmail.com>
2014-04-12 19:26:26 +0800

    fix goTop bug

--- app/scripts/viewLogic/all.js
+++ app/scripts/viewLogic/all.js
@@ -4,6 +4,7 @@
    * module dependence
    */
    var loadimg  = require('loadimg')
+     , toTop = require('toTop')
      , scroll   = require('scroll');
 
 
--- app/scripts/viewLogic/store.js
+++ app/scripts/viewLogic/store.js
@@ -11,6 +11,7 @@
      slide('.page-store #slider','.page-store .menu-bar .btn');
      //themeDetail page slider
      slide('.theme-detail #sub-slider','.theme-detail .sub-menu-bar .btn');
+
    };
 
 })(window,require);

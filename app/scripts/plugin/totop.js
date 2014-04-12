f26865fcee52fba0713fa66144841fe0512b7399
mix <xtmememix@gmail.com>
2014-04-12 15:17:59 +0800

    add back-to-top

---
+++ app/scripts/plugin/totop.js
@@ -0,0 +1,31 @@
+(function(exports){
+
+    function goTop() {
+        $(".layout").scroll(function(e) {
+            //若滚动条离顶部大于100元素
+            if( $(".layout").scrollTop()>100)
+                $("#gotop").fadeIn(1000);//以1秒的间隔渐显id=gotop的元素
+            else
+                $("#gotop").fadeOut(1000);//以1秒的间隔渐隐id=gotop的元素
+        });
+    };
+
+  /**
+   * selector [string]
+   */
+  exports.toTop = function(e) {
+    //点击回到顶部的元素
+    $("#gotop").click(function(e) {
+        //以1秒的间隔返回顶部
+        $(".layout").animate({scrollTop:0},1000);
+    });
+    $("#gotop").mouseover(function(e) {
+        $(this).css("background-position","0 0");
+    });
+    $("#gotop").mouseout(function(e) {
+        $(this).css("background-position","-76px 0");
+    });
+    goTop();//实现回到顶部元素的渐显与渐隐
+  };
+
+})(window);
